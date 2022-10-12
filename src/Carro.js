f90760d9c67ef6cb43642b24ba66a9721dd2ae27
Jessica-star97 <61400580+Jessica-star97@users.noreply.github.com>
2022-10-12 15:01:28 -0500

    1 file modified

--- src/Carro.js
+++ src/Carro.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 export default function Carro(props) {
+    const estiloDiv= {
+        backgroundColor: "lightblue",
+        fontFamily:"Sans-Serif"
+    };
+   
+    const estiloH2 ={
+        color:"red"
+    };
+
+
     return(
-        <div>
-            <h2>Soy un carro modelo {props.marca}</h2>
+        <div style={estiloDiv}> {/*Esto es un comentario*/}
+            <h2 style= {estiloH2}>Soy un carro modelo {props.marca}</h2>
             <img src={props.imagen}
             alt="Imagen del carro"
-            with="150"/>
+            />
             <p> {props.descripcion}</p>
             
         </div>
